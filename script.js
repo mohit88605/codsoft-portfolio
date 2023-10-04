f70f73c35378396324a00fa56587288419b2f21b
Mohit Kumar <mohitkumar88605@gmail.com>
2023-10-04 22:48:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var Typed = new Typed("#text", {
+    strings: ["Frontent Developer", ], // An array of strings to cycle through
+    typeSpeed : 100, // typing speed
+    backSpeed:100,
+    backDelay:100,
+    loop:true 
+});
